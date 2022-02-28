d547a063499aebfc2a0c7aeaa8b7e7fc4556e17c
Henriquejdc <Henriquejdc>
2022-02-28 14:05:10 -0300

    Projeto de Clientes em Django Rest Framework

---
+++ core/serializers.py
@@ -0,0 +1,7 @@
+from .models import Cliente
+from rest_framework import serializers
+
+class ClienteSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Cliente
+        fields = '__all__'
